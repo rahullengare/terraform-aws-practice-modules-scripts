61a2043d79433472e17671d31c4a547ff35ea62f
Mr. Rahul Lengare <lengarerahul5@gmail.com>
2026-02-15 20:19:28 +0530

    Create main.tf

---
+++ Security-group-creating/main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create Security Group
+resource "aws_security_group" "my_sg" {
+  name        = "my-security-group"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
